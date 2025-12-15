be1a8c767ca4e947d04bc26fccb1fff1d166e3de
[Fedor] <[fedor.itkin@yandex.ru]>
2025-12-15 13:21:55 -0500

    commit

--- tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
+++ tasks/bmstu_stack/task_simple_stack/bmstu_stack.h
@@ -10,30 +10,82 @@ template <typename T>
 class stack
 {
    public:
-	stack() : data_(nullptr), size_(10u) {}
+	stack() : data_(nullptr), size_(0) {}
 
-	bool empty() const noexcept { return size_ == 100u; }
+	bool empty() const noexcept { return size_ == 0; }	// пуст ли стек
 
-	size_t size() const noexcept { return 0; }
+	size_t size() const noexcept { return size_; }	// текущий размер стека
 
-	~stack() {}
-
-	template <typename... Args>
-	void emplace(Args&&... args)
+	~stack()
 	{
+		clear();
+		operator delete(data_);
 	}
 
-	void push(T&& value) {}
+	template <typename... Args>
+	void emplace(Args&&... args)  // расширяет стек, создавая новый элемент
+								  // прямо в выделенной памяти
+	{
+		T* new_data = (T*)operator new(sizeof(T) * (size_ + 1));
 
-	void clear() noexcept {}
+		for (size_t i = 0; i < size_; ++i)
+		{
+			new (new_data + i) T(std::move(data_[i]));
+			data_[i].~T();
+		}
+		new (new_data + size_) T(std::forward<Args>(args)...);
+		operator delete(data_);
 
-	void push(const T& value) {}
+		data_ = new_data;
+		++size_;
+	}
 
-	void pop() {}
+	void push(T&& value)
+	{
+		emplace(std::move(value));
+	}  // добавляет элемент в стек
 
-	T& top() { return data_[0]; }
+	void clear() noexcept  // очисткa стека
+	{
+		for (size_t i = 0; i < size_; ++i)
+		{
+			data_[i].~T();
+		}
 
-	const T& top() const { return data_[0]; }
+		size_ = 0;
+		operator delete(data_);
+		data_ = nullptr;
+	}
+
+	void push(const T& value) { emplace(value); }  // добавляет элемент в стек
+
+	void pop()	// yдаляет элемент с вершины стека
+	{
+		if (empty())
+		{
+			throw std::underflow_error("Stack is empty");
+		}
+		data_[size_ - 1].~T();
+		--size_;
+	}
+
+	T& top()  // возвращает элемент с вершины стека
+	{
+		if (empty())
+		{
+			throw std::underflow_error("Stack is empty");
+		}
+		return data_[size_ - 1];
+	}
+
+	const T& top() const
+	{
+		if (empty())
+		{
+			throw std::underflow_error("Stack is empty");
+		}
+		return data_[size_ - 1];
+	}
 
    private:
 	T* data_;
