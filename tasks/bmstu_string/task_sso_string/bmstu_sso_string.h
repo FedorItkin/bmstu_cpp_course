efcda8ad60c66a21bd191f6a125485c74471b9ee
[Fedor] <[fedor.itkin@yandex.ru]>
2026-04-18 03:25:46 -0400

    Second SSO

--- tasks/bmstu_string/task_sso_string/bmstu_sso_string.h
+++ tasks/bmstu_string/task_sso_string/bmstu_sso_string.h
@@ -42,252 +42,80 @@ class basic_string
 	Data data_;
 	bool is_long_;
 
-	bool is_long() const { return is_long_; }
+	bool is_long() const { return false; }
 
-	T* get_ptr()
-	{
-		if (is_long_)
-		{
-			return data_.long_str.ptr;
-		}
-		else
-		{
-			return data_.short_str.buffer;
-		}
-	}
+	T* get_ptr() { return nullptr; }
 
-	const T* get_ptr() const
-	{
-		if (is_long_)
-		{
-			return data_.long_str.ptr;
-		}
-		else
-		{
-			return data_.short_str.buffer;
-		}
-	}
+	const T* get_ptr() const { return nullptr; }
 
-	size_t get_size() const
-	{
-		if (is_long_)
-		{
-			return data_.long_str.size;
-		}
-		else
-		{
-			return data_.short_str.size;
-		}
-	}
+	size_t get_size() const { return 0; }
 
-	size_t get_capacity() const
-	{
-		if (is_long_)
-		{
-			return data_.long_str.capacity;
-		}
-		else
-		{
-			return SSO_CAPACITY;
-		}
-	}
+	size_t get_capacity() const { return 0; }
 
    public:
-	basic_string()
+	basic_string() {}
+
+	basic_string(size_t size) {}
+
+	basic_string(std::initializer_list<T> il) {}
+
+	basic_string(const T* c_str) {}
+
+	basic_string(const basic_string& other) {}
+
+	basic_string(basic_string&& dying) noexcept {}
+
+	~basic_string() {}
+
+	const T* c_str() const { return nullptr; }
+
+	size_t size() const { return 0; }
+
+	bool is_using_sso() const { return false; }
+
+	size_t capacity() const { return 0; }
+
+	basic_string& operator=(basic_string&& other) noexcept { return *this; }
+
+	basic_string& operator=(const T* c_str) { return *this; }
+
+	basic_string& operator=(const basic_string& other) { return *this; }
+
+	friend basic_string<T> operator+(const basic_string<T>& left,
+									 const basic_string<T>& right)
 	{
-		is_long_ = false;
-		data_.short_str.buffer[0] = T('\0');
-		data_.short_str.size = 0;
+		return {};
 	}
 
-	basic_string(size_t size)
+	template <typename S>
+	friend S& operator<<(S& os, const basic_string& obj)
 	{
-		if (size > SSO_CAPACITY)
-		{
-			is_long_ = true;
-			data_.long_str.size = size;
-			data_.long_str.capacity = size * 2;
-			data_.long_str.ptr = new T[data_.long_str.capacity];
-
-			for (size_t i = 0; i < size; i++)
-			{
-				data_.long_str.ptr[i] = T(' ');
-			}
-			data_.long_str.ptr[size] = T('\0');
-		}
-		else
-		{
-			is_long_ = false;
-			data_.short_str.size = size;
-			for (size_t i = 0; i < size; i++)
-			{
-				data_.short_str.buffer[i] = T(' ');
-			}
-			data_.short_str.buffer[size] = T('\0');
-		}
+		return os;
 	}
 
-	basic_string(std::initializer_list<T> il)
+	template <typename S>
+	friend S& operator>>(S& is, basic_string& obj)
 	{
-		size_t size = il.size();
-		if (size > SSO_CAPACITY)
-		{
-			is_long_ = true;
-			data_.long_str.size = size;
-			data_.long_str.capacity = size * 2;
-			data_.long_str.ptr = new T[data_.long_str.capacity];
-		}
-		else
-		{
-			is_long_ = false;
-			data_.short_str.size = size;
-		}
-		size_t i = 0;
-		for (T value : il)
-		{
-			get_ptr()[i] = value;
-			i++;
-		}
-		get_ptr()[size] = T('\0');
+		return is;
 	}
 
-	basic_string(const T* c_str)
+	basic_string& operator+=(const basic_string& other) { return *this; }
+
+	basic_string& operator+=(T symbol) { return *this; }
+
+	T& operator[](size_t index) noexcept
 	{
-		size_t len = strlen_(c_str);
-
-		if (len > SSO_CAPACITY)
-		{
-			is_long_ = true;
-			data_.long_str.size = len;
-			data_.long_str.capacity = len * 2;
-			data_.long_str.ptr = new T[data_.long_str.capacity];
-		}
-		else
-		{
-			is_long_ = false;
-			data_.short_str.size = len;
-		}
-
-		std::copy(c_str, c_str + len, get_ptr());
-		get_ptr()[len] = T('\0');
+		static T dummy;
+		return dummy;
 	}
 
-	basic_string(const basic_string& other)
-	{
-		is_long_ = other.is_long_;
-		if (is_long_)
-		{
-			data_.long_str.size = other.data_.long_str.size;
-			data_.long_str.capacity = other.data_.long_str.capacity;
-			data_.long_str.ptr = new T[data_.long_str.capacity];
-		}
-		else
-		{
-			data_.short_str.size = other.data_.short_str.size;
-			for (int i = 0; i < data_.short_str.size; i++)
-			{
-				data_.short_str.buffer[i] = other.data_.short_str.buffer[i];
-			}
-		}
+	T& at(size_t index) { throw std::out_of_range("Wrong index"); }
 
-		std::copy(other.get_ptr(), other.get_ptr() + other.get_size(),
-				  get_ptr());
+	T* data() { return nullptr; }
 
-		get_ptr()[get_size()] = T('\0');
-	}
-}
+   private:
+	static size_t strlen_(const T* str) { return 0; }
 
-basic_string(basic_string&& dying) noexcept
-{
-}
-
-~basic_string() {}
-
-const T* c_str() const
-{
-	return nullptr;
-}
-
-size_t size() const
-{
-	return 0;
-}
-
-bool is_using_sso() const
-{
-	return false;
-}
-
-size_t capacity() const
-{
-	return 0;
-}
-
-basic_string& operator=(basic_string&& other) noexcept
-{
-	return *this;
-}
-
-basic_string& operator=(const T* c_str)
-{
-	return *this;
-}
-
-basic_string& operator=(const basic_string& other)
-{
-	return *this;
-}
-
-friend basic_string<T> operator+(const basic_string<T>& left,
-								 const basic_string<T>& right)
-{
-	return {};
-}
-
-template <typename S>
-friend S& operator<<(S& os, const basic_string& obj)
-{
-	return os;
-}
-
-template <typename S>
-friend S& operator>>(S& is, basic_string& obj)
-{
-	return is;
-}
-
-basic_string& operator+=(const basic_string& other)
-{
-	return *this;
-}
-
-basic_string& operator+=(T symbol)
-{
-	return *this;
-}
-
-T& operator[](size_t index) noexcept
-{
-	static T dummy;
-	return dummy;
-}
-
-T& at(size_t index)
-{
-	throw std::out_of_range("Wrong index");
-}
-
-T* data()
-{
-	return nullptr;
-}
-
-private:
-static size_t strlen_(const T* str)
-{
-	return 0;
-}
-
-void clean_() {}
+	void clean_() {}
 };
 }  // namespace bmstu
